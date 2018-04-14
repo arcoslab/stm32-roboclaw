d269df1f207b97d0cc1ce710676a9e131655615f
Federico Ruiz Ugalde <memeruiz@gmail.com>
2018-04-13 18:59:10 -0600

    Usart support

--- src/f4/stm32f411discovery/stm32-roboclaw.c
+++ src/f4/stm32f411discovery/stm32-roboclaw.c
@@ -27,16 +27,38 @@
 #include <libopencm3-plus/stm32f4discovery/leds.h>
 #include <limits.h>
 #include <stdbool.h>
+#include <libopencm3/stm32/usart.h>
+
 
 void leds_init(void) {
-	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPDEN);
+	rcc_periph_clock_enable(RCC_GPIOE);
 	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12| GPIO13| GPIO14| GPIO15);
+}
+
+void usart_init(void) {
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2| GPIO3);
+        gpio_set_af(GPIOA, GPIO_AF7, GPIO2 | GPIO3);
+
+	rcc_periph_clock_enable(RCC_USART2);
+
+	/* Setup USART2 parameters. */
+	usart_set_baudrate(USART2, 115200);
+	usart_set_databits(USART2, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX_RX);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(USART2);
 }
 
 void system_init(void) {
   rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_84MHZ]);
   leds_init();
   cdcacm_init();
+  usart_init();
 }
 
 int main(void)
@@ -67,6 +89,7 @@ int main(void)
     	  c=getc(stdin);
     	  i++;
     	  putc(c, stdout);
+          usart_send_blocking(USART2, 'a');
     	}
       }
     }
