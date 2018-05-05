03f8ac676092ef5eef1ecdd3a526066e657a7dc7
Stuart Leal Q <stuart.leal23@gmail.com>
2018-05-05 11:48:00 -0600

    Added license header to file

--- src/f4/stm32f411discovery/roboclaw.c
+++ src/f4/stm32f411discovery/roboclaw.c
@@ -1,9 +1,28 @@
+/*
+ * Copyright (C) 2018 ARCOS-Lab Universidad de Costa Rica
+ * Author: Stuart Leal Quesada <stuart.leal23@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <stdint.h>
 #include <stdio.h>
 #include <libopencm3/stm32/usart.h>
 
 unsigned int crc16(unsigned char *packet, int nBytes) {
-  unsigned int crc;
+  unsigned int crc=0;
   for (int byte = 0; byte < nBytes; byte++) {
     crc = crc ^ ((unsigned int)packet[byte] << 8);
     for (unsigned char bit = 0; bit < 8; bit++) {
@@ -35,19 +54,21 @@ int read_firmware(char* output, uint8_t address) {
 int read_main_battery(char* output, uint8_t address) {
   unsigned int rcv = 0;
 
+  char cmd = 24; // CMD value for read main battery
   usart_send_blocking(USART2, address);
-  usart_send_blocking(USART2, 24);
+  usart_send_blocking(USART2, cmd);
 
-  unsigned char *packet = address + 24;
-  short checksum = crc16(packet, 2);
-  char high_byte = checksum >> 8;
-  char low_byte = checksum;
-  fprintf(stdout, "%u %u", high_byte, low_byte);
-  
-  for(int i=0; i<4; i++) {
+  for(int i=0; i<2; i++) {
     rcv = usart_recv_blocking(USART2);
     fprintf(stdout, " %u" , rcv);
-  }
+  } // first two bytes are the battery voltage
+
+  unsigned char crc_rcv[2]; // received crc value from roboclaw
+  data[1] = usart_recv_blocking(USART2);
+  data[0] = usart_recv_blocking(USART2);
+
+  fprintf(stdout, " %u", data[1]);
+  fprintf(stdout, " %u". data[0]);
 }
 
 
