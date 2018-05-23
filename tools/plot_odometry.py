cbc1416ba18aafc9306f3b81cfeccfb08338b89e
Stuart Leal Q <stuart.leal23@gmail.com>
2018-05-22 21:35:11 -0600

    Add matplotlib to graphic the output of vel.

--- tools/plot_odometry.py
+++ tools/plot_odometry.py
@@ -3,29 +3,51 @@ import re
 import serial
 import sys
 import select
+import matplotlib.pyplot as plt
+import collections
 
 def enter(ser):
     ser.write("\r".encode('utf-8'))
 
-def process_line(ser, pattern):
+def process_line(ser, pattern, line1, fig):
     line = ser.readline().decode('utf-8')
     match = re.match(pattern, line)
     if match:
         print(match.group('vel'))
+        fig_update(float(match.group('vel')), line1, fig)
+
+def update():
+    y = match.group('vel')
+    y = float(y)/1000
+    plt.plot(i+30, y, marker='.')
+    plt.pause(0.02)
+    plt.axis([0+i, 30+i, 0, 1])
 
 def write_char(char,ser):
     ser.write(char.encode('utf-8'))
     enter(ser)
 
+def fig_update(vel, line1, fig):
+    vels.append(vel)
+    line1.set_ydata(list(vels))
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+
+plt.ion()
+vels = collections.deque([0 for number in range(50)], maxlen=50)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+x = collections.deque(range(50), maxlen=50)
+vels.append(1700) # this sets the graph size
+line1, = ax.plot(list(x), list(vels), 'b-')
 ser = serial.Serial('/dev/ttyACM0')
 ser.baudrate = 115200
 pattern = re.compile(r'.*Past\s*Pos:\s*(?P<ppos> \d+)\s*\|\s*Current\s*Pos:\s*(?P<cpos> \d+)\s*\|\s*TICKS\s*TIME:\s*\d+\.\d+\s*\|\s*Counter:\s*(?P<counter> \d+)\s*\|\s*Current\s*Vel:\s*(?P<vel> -?\d+\.\d+).*')
-
 enter(ser)
 
 while(1):
-    process_line(ser, pattern)
-    while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+    process_line(ser, pattern, line1, fig)
+    while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:# this is like a poll to stdin. flush with enter
         char = sys.stdin.read(1)
         if(char):
             write_char(char, ser)
