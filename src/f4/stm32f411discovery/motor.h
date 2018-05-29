c426025ae7d4e60e837050ff4ec2cf18e88b117e
Stuart Leal Q <stuart.leal23@gmail.com>
2018-05-29 17:41:51 -0600

    Add timer type to motor struct. This will help in keeping odometry for each motor.

--- src/f4/stm32f411discovery/motor.h
+++ src/f4/stm32f411discovery/motor.h
@@ -4,18 +4,20 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "encoder.h"
+#include "timers.h"
 #include "usart.h"
+
 
 typedef struct motor {
   /* Virtual make motors appears as different
    * and the contents inside this struct will manage
    * to use the right usart com port and address
   */
-  uint32_t usart; // com port defined by the usart init
   uint8_t address; // address to corresponding roboclaw in that port
   bool code; // each roboclaw has two motors. Choose between them
-//  encoder encoder; // encoder related to this motor
+  encoder encoder; // encoder related to this motor
   usart_port port; // usart port where the roboclaw for this motor is
+  timer encoder_timer; // timer in stm for the encoder
 } motor;
 
 #endif
